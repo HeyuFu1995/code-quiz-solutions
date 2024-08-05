c63cb602544e6db9c9957e6d1999f37d3b8a629a
Heyu Fu <heyufu2023@gmail.com>
2024-08-05 14:48:33 +1000

    SetMatrixZeros73 the Second solution added Reduce the Space Complexity to O(1).

--- src/main/java/tradition150/SetMatrixZeros73.java
+++ src/main/java/tradition150/SetMatrixZeros73.java
@@ -12,7 +12,7 @@ public class SetMatrixZeros73 {
     }
 
     /**
-     * Solution
+     * Solution 1
      * Time Complexity: O(mn)
      * @param matrix
      */
@@ -35,6 +35,41 @@ public class SetMatrixZeros73 {
             for(int rowNum = 0; rowNum < rowLength; rowNum++) {
                 if (colSet.contains(colNum) || rowSet.contains(rowNum)) {
                     matrix[colNum][rowNum] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * Solution 2
+     * Time Complexity: O(mn)
+     * Reduce the Space Complexity to O(1)
+     * @param matrix
+     */
+    public void setZeroes2(int[][] matrix) {
+        int colLength = matrix.length;
+        int rowLength = matrix[0].length;
+        boolean flagCol0 = false;
+
+        for(int col = 0; col < colLength; col++) {
+            if(matrix[col][0] == 0) {
+                flagCol0 = true;
+            }
+            for(int row = 1; row < rowLength; row++) {
+                if(matrix[col][row] == 0) {
+                    matrix[col][0] = 0;
+                    matrix[0][row] = 0;
+                }
+            }
+        }
+
+        for(int col = colLength - 1; col >= 0; col--) {
+            for(int row = 1; row < rowLength; row++) {
+                if(matrix[col][0] == 0 || matrix[0][row] == 0) {
+                    matrix[col][row] = 0;
+                }
+                if(flagCol0) {
+                    matrix[col][0] = 0;
                 }
             }
         }
