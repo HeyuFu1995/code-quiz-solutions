f503a20c865679c9af393b2a9f66580e7a6c26b0
Heyu Fu <heyufu2023@gmail.com>
2024-08-08 11:27:31 +1000

    MajorityElement169 solution 2 added reduce time complexity to O(n).

--- src/main/java/tradition150/MajorityElement169.java
+++ src/main/java/tradition150/MajorityElement169.java
@@ -20,4 +20,27 @@ public class MajorityElement169 {
         Arrays.sort(nums);
         return nums[nums.length / 2];
     }
+
+    /**
+     * Solution 2
+     * Time Complexity: O(n)
+     * @param nums
+     * @return
+     */
+    public int majorityElement2(int[] nums) {
+        int times = 1;
+        int num = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == num) {
+                times++;
+            } else if(times > 0) {
+                times--;
+            } else {
+                times = 1;
+                num = nums[i];
+            }
+        }
+        return num;
+    }
 }
