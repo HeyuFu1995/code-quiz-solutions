0df9bb0a30bbcc9f9fa5b93998282f668dd27bce
Heyu Fu <heyufu2023@gmail.com>
2024-08-10 12:37:06 +1000

    RotateArray189 solution 2 & 3 added, 2 reduce time complexity to O(n) but increase space complexity to O(n), 3 reduce time complexity to O(n) while no increased space complexity.

--- src/main/java/tradition150/RotateArray189.java
+++ src/main/java/tradition150/RotateArray189.java
@@ -12,7 +12,7 @@ public class RotateArray189 {
     }
 
     /**
-     * Solution
+     * Solution 1
      * Time Complexity: O(n^k)
      * @param nums
      * @param k
@@ -30,4 +30,50 @@ public class RotateArray189 {
             nums[1] = temp;
         }
     }
+
+    /**
+     * Solution 2
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     * @param nums
+     * @param k
+     */
+    public void rotate2(int[] nums, int k) {
+        int length = nums.length;
+        int[] tempArray = new int[length];
+        for(int i = 0; i < length; i++) {
+            tempArray[(i + k) % length] = nums[i];
+        }
+        System.arraycopy(tempArray, 0, nums, 0, length);
+    }
+
+    /**
+     * Solution 3
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     * @param nums
+     * @param k
+     */
+    public void rotate3(int[] nums, int k) {
+        int length = nums.length;
+        k %= length;
+        //Reverse Array
+        reverse(nums, 0, length - 1);
+        //Reverse former half
+        reverse(nums, 0, k - 1);
+        //Reverse latter half
+        reverse(nums, k, length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
 }
