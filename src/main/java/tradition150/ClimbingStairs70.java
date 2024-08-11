e1cdfe71c7a2b7595005e7f2d780f8a73310b663
Heyu Fu <heyufu2023@gmail.com>
2024-08-11 14:46:14 +1000

    ClimbingStairs70 solution 3 added Dynamic Programming reduce Space Complexity to O(1).

--- src/main/java/tradition150/ClimbingStairs70.java
+++ src/main/java/tradition150/ClimbingStairs70.java
@@ -52,4 +52,29 @@ public class ClimbingStairs70 {
         }
         return resultArray[n - 1];
     }
+
+    /**
+     * Solution 2
+     * Advanced Dynamic Programming
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     * @param n
+     * @return
+     */
+    public int climbStairs3(int n) {
+        if(n == 2) {
+            return 2;
+        }
+        if(n == 1) {
+            return 1;
+        }
+        int result1 = 1;
+        int result2 = 2;
+        for(int i = 2; i < n; i++) {
+            int result = result1 + result2;
+            result1 = result2;
+            result2 = result;
+        }
+        return result2;
+    }
 }
