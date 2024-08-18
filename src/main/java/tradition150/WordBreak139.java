c9d3b93690aed8c1ba82adb53c60b990e7e80a4f
Heyu Fu <heyufu2023@gmail.com>
2024-08-18 19:50:45 +1000

    WordBreak139 solution 2 added use DP reduce the time complexity.

--- src/main/java/tradition150/WordBreak139.java
+++ src/main/java/tradition150/WordBreak139.java
@@ -1,9 +1,6 @@
 package tradition150;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WordBreak139 {
 
@@ -16,7 +13,7 @@ public class WordBreak139 {
 
     /**
      * Solution
-     * Time Complexity: O(mn) m = s.length n = wordDict.size()
+     * Time Complexity: O(mn^2) m = s.length n = wordDict.size()
      * @param s
      * @param wordDict
      * @return
@@ -46,5 +43,28 @@ public class WordBreak139 {
             isResult = isResult || backtrack(s, wordDict, curStr + dicStr);
         }
         return isResult;
+    }
+
+    /**
+     * Solution 2
+     * USE DP reduce the complexity
+     * Time Complexity: O(n^2)
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public boolean wordBreak2(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for(int i = 0; i <= s.length(); i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
     }
 }
