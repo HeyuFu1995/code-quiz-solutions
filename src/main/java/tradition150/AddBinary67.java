ec57ec8fc1e44c68973db93e7ebb24eedfbbe267
Heyu Fu <heyufu2023@gmail.com>
2024-08-21 17:35:24 +1000

    AddBinary67 solution 2 added get rid of int and long limitation.

--- src/main/java/tradition150/AddBinary67.java
+++ src/main/java/tradition150/AddBinary67.java
@@ -15,7 +15,7 @@ public class AddBinary67 {
 
     /**
      * Solution
-     * Time Complexit: O(log(a +b))
+     * Time Complexity: O(log(a +b))
      * @param a
      * @param b
      * @return
@@ -24,5 +24,36 @@ public class AddBinary67 {
         long aNum = Long.parseLong(a, 2);
         long bNum = Long.parseLong(b, 2);
         return Long.toBinaryString(aNum + bNum);
+    }
+
+    /**
+     * Solution 2
+     * Time Complexity: O(max(a,b))
+     * @param a
+     * @param b
+     * @return
+     */
+    public String addBinary2(String a, String b) {
+        StringBuffer result = new StringBuffer();
+        boolean carry = false;
+        int lengthA = a.length();
+        int lengthB = b.length();
+        int maxLength = Math.max(lengthB, lengthA);
+        for(int i = 0; i < maxLength; i++) {
+            int sum = carry ? 1 : 0;
+            if(i < lengthA) {
+                sum += a.charAt(lengthA - i - 1) - '0';
+            }
+            if(i < lengthB) {
+                sum += b.charAt(lengthB - i - 1) - '0';
+            }
+            result.insert(0, sum % 2);
+            carry = sum / 2 != 0;
+
+        }
+        if(carry) {
+            result.insert(0, "1");
+        }
+        return result.toString();
     }
 }
