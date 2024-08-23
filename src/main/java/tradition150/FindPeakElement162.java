89da47ca88f0b7866d7d30b797bf8228291cfa5c
Heyu Fu <heyufu2023@gmail.com>
2024-08-23 11:22:23 +1000

    FindPeakElement162 solution added bug fixed margin value processes.

--- src/main/java/tradition150/FindPeakElement162.java
+++ src/main/java/tradition150/FindPeakElement162.java
@@ -1,5 +1,9 @@
 package tradition150;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FindPeakElement162 {
 
     private class TestCase {
@@ -10,20 +14,45 @@ public class FindPeakElement162 {
 
     }
 
+    /**
+     * Solution
+     * Time Complexity: O(logn)
+     * @param nums
+     * @return
+     */
     public int findPeakElement(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-        while(left < right) {
+
+        while(left <= right) {
             int mid = (left + right) / 2;
-            if(mid == 0 || nums[mid] <= nums[mid - 1]) {
-                right = mid;
-            }
-            else if(mid == nums.length - 1 || nums[mid] <= nums[mid + 1]) {
-                left = mid;
+            if(mid == 0) {
+                if(mid != nums.length - 1) {
+                    if (nums[mid + 1] < nums[mid]) {
+                        return mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                } else {
+                    return mid;
+                }
+            } else if(mid == nums.length - 1) {
+                if(nums[mid - 1] < nums[mid]) {
+                    return mid;
+                } else {
+                    right = mid - 1;
+                }
             } else {
-                left++;
+                if (nums[mid - 1] < nums[mid] && nums[mid + 1] < nums[mid]) {
+                    return mid;
+                }
+                if (nums[mid + 1] > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-        return left;
+        return 0;
     }
 }
