745e3c275551ed5c02aee423a508ec382ad23ba6
Heyu Fu <heyufu2023@gmail.com>
2024-08-24 13:35:46 +1000

    ReverseWordsInAString151 solution 2 & 3 added.

--- src/main/java/tradition150/ReverseWordsInAString151.java
+++ src/main/java/tradition150/ReverseWordsInAString151.java
@@ -1,5 +1,10 @@
 package tradition150;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
 public class ReverseWordsInAString151 {
     private class TestCase {
         static String s = "the sky is blue";
@@ -26,6 +31,49 @@ public class ReverseWordsInAString151 {
         }
         if(!builder.isEmpty()) {
             builder.deleteCharAt(builder.length() - 1);
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Solution 2
+     * @param s
+     * @return
+     */
+    public String reverseWords2(String s) {
+        s = s.trim();
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+
+    public static void main(String[] args) {
+        new ReverseWordsInAString151().reverseWords3("t ");
+    }
+
+    /**
+     * Solution 3
+     * @param s
+     * @return
+     */
+    public String reverseWords3(String s) {
+        Stack<String> stringStack = new Stack<>();
+        int start = -1;
+        for(int i = 0; i < s.length(); i++) {
+            if(start == -1 && s.charAt(i) != ' '){
+                start = i;
+            }
+            if(start != -1 && (s.charAt(i) == ' ' || i == s.length() - 1)) {
+                stringStack.push(i == s.length() - 1 && s.charAt(i) != ' '? s.substring(start) : s.substring(start, i));
+                start = -1;
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        while(!stringStack.isEmpty()) {
+            builder.append(stringStack.pop());
+            if(!stringStack.isEmpty()) {
+                builder.append(" ");
+            }
         }
         return builder.toString();
     }
