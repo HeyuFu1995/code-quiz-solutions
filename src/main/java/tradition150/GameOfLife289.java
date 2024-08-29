4f5a42b1b63b4a6aed6023eb33f16f9adedca7d5
Heyu Fu <heyufu2023@gmail.com>
2024-08-29 16:16:38 +1000

    GameOfLife289 solution added.

---
+++ src/main/java/tradition150/GameOfLife289.java
@@ -0,0 +1,58 @@
+package tradition150;
+
+public class GameOfLife289 {
+    private class TestCase {
+
+        static int[][] board = {{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
+    }
+
+    public static void solution() {
+
+    }
+
+    /**
+     * Solution
+     * Time Complexity: O(m * n)
+     * @param board
+     */
+    public void gameOfLife(int[][] board) {
+        int[] neighbors = {0, -1, 1};
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        int[][] copyBoard = new int[rows][cols];
+
+        for(int row = 0; row < rows; row++) {
+            for(int col = 0; col < cols; col++) {
+                copyBoard[row][col] = board[row][col];
+            }
+        }
+
+        for(int row = 0; row < rows; row++) {
+            for(int col = 0; col < cols; col++) {
+                int liveNeighbors = 0;
+                for(int i = 0; i < 3; i++) {
+                    for(int j = 0; j < 3; j++) {
+                        if(!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            int r = row + neighbors[i];
+                            int c = col + neighbors[j];
+                            if((r < rows && r >= 0) && (c < cols && c >= 0) && (copyBoard[r][c] == 1)) {
+                                liveNeighbors++;
+                            }
+                        }
+                    }
+                }
+
+                if(copyBoard[row][col] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)){
+                    board[row][col] = 0;
+                }
+                if(copyBoard[row][col] == 0 && liveNeighbors == 3) {
+                    board[row][col] = 1;
+                }
+            }
+        }
+    }
+
+
+}
