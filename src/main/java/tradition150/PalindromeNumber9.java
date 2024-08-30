9aedaeb4341ef0346b30e881c7d9e3a75404d414
Heyu Fu <heyufu2023@gmail.com>
2024-08-30 14:15:22 +1000

    PalindromeNumber9 solution 2 added reduce time complexity to O(log(n)).

--- src/main/java/tradition150/PalindromeNumber9.java
+++ src/main/java/tradition150/PalindromeNumber9.java
@@ -32,4 +32,24 @@ public class PalindromeNumber9 {
         }
         return true;
     }
+
+    /**
+     * Solution 2
+     * Time Complexity: O(log(n))
+     * @param x
+     * @return
+     */
+    public boolean isPalindrome2(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
 }
