c9dde54f8e5032fb73430dd49d1e6fa63bdb628c
Heyu Fu <heyufu2023@gmail.com>
2024-08-30 13:47:03 +1000

    SearchInsertPosition35 solution2 added.

--- src/main/java/tradition150/SearchInsertPosition35.java
+++ src/main/java/tradition150/SearchInsertPosition35.java
@@ -22,10 +22,33 @@ public class SearchInsertPosition35 {
      */
     public int searchInsert(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++) {
-            if(nums[i] > target) {
+            if(nums[i] >= target) {
                 return i;
             }
         }
         return nums.length;
+    }
+
+
+    /**
+     * Solution 2
+     * Time Complexity: O(log n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 }
