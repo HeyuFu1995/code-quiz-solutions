dd947e318fc81bb58911e740e111d1bd0595639c
Heyu Fu <heyufu2023@gmail.com>
2024-08-31 18:31:12 +1000

    NumberOf1Bits191 solution2 & 3 added 1 principle added.

--- src/main/java/tradition150/NumberOf1Bits191.java
+++ src/main/java/tradition150/NumberOf1Bits191.java
@@ -13,11 +13,48 @@ public class NumberOf1Bits191 {
 
     /**
      * Solution
-     * Time Complexity: O(log n)
+     * Time Complexity: O(1)
      * @param n
      * @return
      */
     public int hammingWeight(int n) {
+//        n = n - ((n >>> 1) & 0x55555555);
+//        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+//        n = (n + (n >>> 4)) & 0x0f0f0f0f;
+//        n = n + (n >>> 8);
+//        n = n + (n >>> 16);
+//        return n & 0x3f;
         return Integer.bitCount(n);
+    }
+
+    /**
+     * Solution 2
+     * Time Complexity: O(32) = O(1)
+     * @param n
+     * @return
+     */
+    public int hammingWeight2(int n) {
+        int ret = 0;
+        for(int i = 0; i < 32; i++) {
+            if((n & (1 << i)) == 1) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Solution 3
+     * Time Complexity: O(log n)
+     * @param n
+     * @return
+     */
+    public int hammingWeight3(int n) {
+        int ret = 0;
+        while(n != 0) {
+            n &= n - 1;
+            ret++;
+        }
+        return ret;
     }
 }
