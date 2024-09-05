0fa3cf4c0b5d65954b2f6f6eacb171612558109e
Heyu Fu <heyufu2023@gmail.com>
2024-09-05 17:59:06 +1000

    InvertBinaryTree226 solution added.

---
+++ src/main/java/tradition150/InvertBinaryTree226.java
@@ -0,0 +1,49 @@
+package tradition150;
+
+public class InvertBinaryTree226 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private class TestCase {
+
+    }
+
+    public static void solution() {
+
+    }
+
+    /**
+     * Solution DFS
+     * Time Complexity: O(n)
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
