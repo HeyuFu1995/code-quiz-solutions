b91098ed2b27e2e7ac7856c022b382994e1c983e
Heyu Fu <heyufu2023@gmail.com>
2024-09-07 15:48:32 +1000

    CopyListWithRandomPointer138 solution 2 added.

--- src/main/java/tradition150/CopyListWithRandomPointer138.java
+++ src/main/java/tradition150/CopyListWithRandomPointer138.java
@@ -55,4 +55,43 @@ public class CopyListWithRandomPointer138 {
         return newOldMap.get(result);
     }
 
+    /**
+     * Solution 2
+     * Time Complexity: O(n)
+     * @param head
+     * @return
+     */
+    public Node copyRandomList2(Node head) {
+        if(head == null) {
+            return null;
+        }
+        Node cur = head;
+        while(cur != null) {
+            Node newNode = new Node(cur.val);
+            newNode.next = cur.next;
+            cur.next = newNode;
+            cur = cur.next.next;
+        }
+
+        cur = head;
+        while(cur != null) {
+            if(cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+        cur = head;
+        Node answer = head.next;
+        Node a = answer;
+        while(cur != null) {
+            cur.next = cur.next.next;
+            cur = cur.next;
+            if(answer.next != null) {
+                answer.next = answer.next.next;
+            }
+            answer = answer.next;
+        }
+        return a;
+    }
+
 }
