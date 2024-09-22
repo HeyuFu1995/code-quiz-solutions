da6400c777a3b1de25d1cef874d58a793b8e3da6
Heyu Fu <heyufu2023@gmail.com>
2024-09-22 20:30:40 +1000

    ProductOfArrayExceptSelf238 solution 2 added.

--- src/main/java/tradition150/ProductOfArrayExceptSelf238.java
+++ src/main/java/tradition150/ProductOfArrayExceptSelf238.java
@@ -29,4 +29,27 @@ public class ProductOfArrayExceptSelf238 {
         }
         return ans;
     }
+
+    /**
+     * Solution 2
+     * Time Complexity: O(n)
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf2(int[] nums) {
+        int length = nums.length;
+        int[] ans = new int[length];
+
+        ans[0] = 1;
+        for(int i = 1; i < length; i++) {
+            ans[i] = nums[i - 1] * ans[i - 1];
+        }
+
+        int r = 1;
+        for(int i = length - 1; i >= 0; i--) {
+            ans[i] = ans[i] * r;
+            r *= nums[i];
+        }
+        return ans;
+    }
 }
